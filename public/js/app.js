5b8d048f7b08124438f847350ffe1d0f2e6c65a3
Andrés León <andresleondoylet@gmail.com>
2021-11-15 21:41:28 -0500

    Testing

--- public/js/app.js
+++ public/js/app.js
@@ -46993,8 +46993,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Andrés León\Documents\GitHub\Galápagos\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Andrés León\Documents\GitHub\Galápagos\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Andrés León\Documents\GitHub\Galápagos\Galapagos\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Andrés León\Documents\GitHub\Galápagos\Galapagos\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
